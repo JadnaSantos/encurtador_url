68784707faf04ea272c6642d27c95ce4fe150739
Jadna Santos <jadnajesus7@gmail.com>
2025-08-07 20:59:01 -0300

    feat: implementa os enpoints para criar um url e buscar url

--- main.go
+++ main.go
@@ -1,3 +1,39 @@
 package main
 
-func main () {}
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/JadnaSantos/Encurtador-de-Url/api"
+)
+
+func main () {
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err) 
+		return 
+	}
+
+	slog.Info("all systems offline")
+}
+
+
+func run () error {
+	db := make(map[string]string)
+
+	handle := api.NewHandler(db)
+
+	s := http.Server{
+		Addr:                         ":8080",
+		Handler:                      handle,	
+		ReadTimeout:                  10 * time.Second,
+		WriteTimeout:                 10 * time.Second,
+		IdleTimeout:                  time.Minute,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
